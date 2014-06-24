92d24eff8a2d1131918f786bd43478eb4aefa1e7
Bud Parr <budparr@sonnetmedia.net>
2014-06-24 10:09:35 -0400

    update scripts

---
+++ src/_includes/scripts/show-hide.js
@@ -0,0 +1,1 @@
+function unhide(divID) {var item = document.getElementById(divID);if (item) {item.style.display=(item.style.display=='none')?'block':'none';}}
---
+++ src/assets/js/scripts.js
@@ -0,0 +1,9 @@
+---
+---
+{% include scripts/vendor/underscore-min.js %}
+{% include scripts/moment/min/moment.min.js %}
+{% include scripts/events-list.js %}
+{% include scripts/responsive-menu.js %}
+{% include scripts/listjs/dist/list.min.js %}
+{% include scripts/show-hide.js %}
+{% include scripts/super-quotes.js  %}
